5248e1d989b8f9053d1e2b9e1024760ea0da6199
Barath Kumar <126680251+kumzalot@users.noreply.github.com>
2024-04-03 19:31:20 +0530

    Add files via upload

---
+++ QuizApp/app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,44 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String[] question ={
+            "Which company owns android?",
+            "Which one is not a programming language?",
+            "Which company owns Apple?",
+            "What is the capital of France?",
+            "Which planet is known as the Red Planet?",
+            "Who wrote 'To Kill a Mockingbird'?",
+            "What is the chemical symbol for water?",
+            "What is the largest mammal in the world?",
+            "Who painted the Mona Lisa?",
+            "Which is the longest river in the world?"
+    };
+
+    public static String[][] choices = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"London", "Berlin", "Paris", "Rome"},
+            {"Mars", "Jupiter", "Venus", "Mercury"},
+            {"Harper Lee", "J.K. Rowling", "Stephen King", "Mark Twain"},
+            {"H2O", "CO2", "NaCl", "O2"},
+            {"Elephant", "Blue Whale", "Giraffe", "Hippopotamus"},
+            {"Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Michelangelo"},
+            {"Nile", "Amazon", "Mississippi", "Yangtze"}
+    };
+
+    public static String[] correctAnswers = {
+            "Google",
+            "Notepad",
+            "Apple",
+            "Paris",
+            "mars",
+            "Harper Lee",
+            "H2O",
+            "Blue Whale",
+            "Leonardo da Vinci",
+            "Nile"
+    };
+
+}
